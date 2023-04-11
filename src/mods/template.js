b9038b259449080adac4eefc7bed3395acaeca2b
ZoltarML <61130715+ZoltarML@users.noreply.github.com>
2023-04-11 14:28:34 -0500

    Create template.js

---
+++ src/mods/template.js
@@ -0,0 +1,36 @@
+// This is a template for adding a mod
+function templateMain() {
+    // Create a local storage entry
+    let exampleSettings = localStorage.getItem('examplesettings');
+    exampleSettings = JSON.parse(exampleSettings);
+
+    // Assign mod shorthand 
+    ig.game.settings.exp = exampleSettings === null ? false : exampleSettings;
+
+    // Create a togglable function
+    ig.game.settings.exampleFunction = function () {
+        if (!this.exp) {
+            // Enable
+            ig.game.gravity = 300;
+        } else {
+            // Disable
+            ig.game.gravity = 800;
+        }
+        this.exp = !this.exp;
+        localStorage.setItem('example', this.exp);
+
+    }
+
+    // On refresh instructions
+    if (ig.game.settings.exp) {
+        ig.game.settings.exp = !ig.game.settings.exp;
+        ig.game.settings.exampleFunction();
+
+    }
+
+    // Settings menu visuals
+    ig.game.settings.Create.toggle("Name here", "exp", "ig.game.settings.exampleFunction()");
+    ig.game.settings.Create.addendum("Just gravity toggle", 'black');
+
+
+}
