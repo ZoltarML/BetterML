0e1e4b431d1ee3a0d16fc91b7a625e0328dc08b2
ZoltarML <61130715+ZoltarML@users.noreply.github.com>
2023-04-13 15:20:14 -0500

    Update inject.js

--- src/inject.js
+++ src/inject.js
@@ -2,7 +2,7 @@
 (function () {
 
     // loader.js MUST come last and extrasounds.js MUST be first
-    let mods = ['extrasounds.js', 'babel.js', 'exporter.js', 'rankhand.js', 'performance.js', 'friendsplus.js', 'movepainter.js', 'norefresh.js', 'freecam.js', 'loader.js'];
+    let mods = ['extrasounds.js', 'babel.js', 'exporter.js', 'pixelCopyImage.js', 'rankhand.js', 'performance.js', 'friendsplus.js', 'movepainter.js', 'norefresh.js', 'freecam.js', 'importer.js', 'loader.js'];
 
     for (mod of mods) {
         let node = document.getElementsByTagName('body')[0];
